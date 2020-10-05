ac4f13077e82dcd632cc98b9a6f103265d664f49
Jonas Thelemann <e-mail@jonas-thelemann.de>
2020-10-05 03:20:05 +0200

    feat: add timestamp

--- main.js
+++ main.js
@@ -17,8 +17,20 @@ exec('git rev-list --tags --max-count=1', (err, rev, stderr) => {
 
         tag = tag.trim()
 
-        console.log('\x1b[32m%s\x1b[0m', `Found tag: ${tag}`);
-        console.log(`::set-output name=tag::${tag}`);
-        process.exit(0);
+        exec(`git log -1 --format=%at ${tag}`, (err, timestamp, stderr) => {
+            if (err) {
+                console.log('\x1b[33m%s\x1b[0m', 'Could not find any timestamp because: ');
+                console.log('\x1b[31m%s\x1b[0m', stderr);
+                process.exit(1);
+            }
+
+            timestamp = timestamp.trim()
+
+            console.log('\x1b[32m%s\x1b[0m', `Found tag: ${tag}`);
+            console.log('\x1b[32m%s\x1b[0m', `Found timestamp: ${timestamp}`);
+            console.log(`::set-output name=tag::${tag}`);
+            console.log(`::set-output name=timestamp::${timestamp}`);
+            process.exit(0);
+        });
     });
 });
